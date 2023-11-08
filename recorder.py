05fc628ed0a8e06501bd95e87b9152c220d7f856
CrazySqueak <crazysqueak@gmail.com>
2023-11-08 12:12:55 +0000

    Initial commit.

---
+++ recorder.py
@@ -0,0 +1,222 @@
+import subprocess
+import time
+import threading
+import os
+import queue
+import itertools
+from multiprocessing.pool import ThreadPool
+import _thread
+import sys
+
+from globalsettings import OUTFILES_ROOT, RECENT_ROOT, ARCHIVE_ROOT, SURVEY_ROOT, LATEST_ROOT, FILENAME_FMT
+
+from rich.console import Console
+from rich.progress import Progress, SpinnerColumn, TextColumn, TimeElapsedColumn
+console = Console()
+progress = Progress(
+    SpinnerColumn(),
+    TextColumn("[progress.description]{task.description}"),
+    TimeElapsedColumn(),
+    
+    console=console)
+
+# Get ffmpeg executable
+from shutil import which
+EXECUTABLE = which("ffmpeg")
+if not EXECUTABLE:
+    raise FileNotFoundError("Unable to locate FFmpeg executable! Please ensure that it is installed and on your $PATH!")
+print("Using FFMPEG:", EXECUTABLE)
+
+# Work out what platform we're on
+import platform
+SYSTEM = platform.system()
+if SYSTEM == "Linux":
+    print("Current System: Linux")
+    DRIVER = "v4l2"
+    LIST_CMD = "v4l2-ctl --list-devices"
+elif SYSTEM == "Windows":
+    print("Current System: Windows")
+    DRIVER = "dshow"
+    LIST_CMD = "ffmpeg -list_devices true -f dshow -i dummy"
+else:
+    print("Current System: Unrecognised.")
+    print("No support is implemented for ", SYSTEM)
+
+# Init video device
+DEVICES = ["/dev/video0"]
+if not DEVICES:
+    print("No device selected.")
+    print("Printing list of devices:")
+    os.system(LIST_CMD)
+    print("Please configure your devices in the program configuration and then restart the program.")
+    assert False
+print("Selected Devices:", ", ".join(DEVICES))
+FFMPEG_DEVICE_ARGS = [x for device in DEVICES for x in ("-i", device)]  # FLATMAP WOO!!
+
+MAX_RECENT = 24
+MAX_FULLFILE_LEN = 3600
+
+def mux_video(src):
+    muxed_file = os.path.join(RECENT_ROOT, os.path.splitext(os.path.basename(src))[0]) + ".mp4"
+    if not os.path.exists(src): return
+
+    tid = progress.add_task(description=f"Muxing {os.path.basename(src)}...", total=None)
+    try:
+        ffmpeg = subprocess.run([EXECUTABLE,
+                                   "-y",
+                                   "-i", src,
+                                   "-c:v", "copy",
+                                   "-c:a", "copy",
+                                   muxed_file],
+                                check=True,
+                                stdout=subprocess.DEVNULL,
+                                stderr=subprocess.STDOUT)
+        assert ffmpeg.returncode == 0
+    except:
+        progress.remove_task(tid)
+        console.log(f"[red]FFMPEG crashed! Unable to mux {src}!", highlight=False)
+        os.remove(muxed_file)
+        return
+
+    os.remove(src)  # Delete old video
+    progress.remove_task(tid)
+    console.log("Muxed:", os.path.basename(muxed_file), highlight=False)
+    
+def archive_video(src):
+    archive_file = os.path.join(ARCHIVE_ROOT, os.path.splitext(os.path.basename(src))[0]) + ".mp4"
+    if not os.path.exists(src): return
+
+    tid = progress.add_task(description=f"Archiving {os.path.basename(src)}...", total=None)
+    try:
+        ffmpeg = subprocess.run([EXECUTABLE,
+                                   "-y",
+                                   "-i", src,
+                                   "-vf", "scale=-2:360",
+                                   "-c:a", "copy",
+                                   archive_file],
+                                check=True,
+                                stdout=subprocess.DEVNULL,
+                                stderr=subprocess.STDOUT)
+        assert ffmpeg.returncode == 0
+    except:
+        progress.remove_task(tid)
+        console.log(f"[red]FFMPEG crashed! Unable to archive {src}!", highlight=False)
+        os.remove(archive_file)
+        return
+
+    os.remove(src)  # Delete old video
+    progress.remove_task(tid)
+    console.log("Archived:", os.path.basename(archive_file), highlight=False)
+
+
+def get_survey_name(src):
+    return os.path.join(SURVEY_ROOT, os.path.splitext(os.path.basename(src))[0]) + ".gif"
+SURVEY_FPS = 4
+SURVEY_SPEED = 60
+MAX_SURVEY_GIFS = 100
+def gengif(src):
+    gif_file = get_survey_name(src)
+    if os.path.exists(gif_file): return 
+
+    tid = progress.add_task(description=f"Generating survey gif for {os.path.basename(src)}...", total=None)
+    try:
+        ffmpeg = subprocess.run([EXECUTABLE,
+                                   "-n",
+                                   "-i", src,
+                                 "-vf", f"setpts=PTS/{SURVEY_SPEED},fps={SURVEY_FPS},scale=-2:360:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse",
+                                 "-loop", "0",
+                                 gif_file
+                                 ],
+                                check=True,
+                                stdout=subprocess.DEVNULL,
+                                stderr=subprocess.DEVNULL)
+        assert ffmpeg.returncode == 0
+    except:
+        progress.remove_task(tid)
+        console.log(f"[red]FFMPEG crashed! Unable to create survey gif for {src}!", highlight=False)
+        os.remove(gif_file)
+        return
+    
+    progress.remove_task(tid)
+    console.log("Created Survey Gif:", os.path.basename(gif_file), highlight=False)
+
+def update_video_state():
+    # Muxing: Latest -> Recent
+    targets = []
+    for fn in sorted(os.listdir(LATEST_ROOT), reverse=True):
+        console.log("Scheduling", fn, "to be muxed.", highlight=False)
+        targets.append(os.path.join(LATEST_ROOT, fn))
+    video_processor.map(mux_video, targets)
+
+    # Archiving: Recent -> Archive
+    if len(os.listdir(RECENT_ROOT)) > MAX_RECENT:
+        # Too many recent files
+        num_excess = len(os.listdir(RECENT_ROOT)) - MAX_RECENT
+        oldest_files = sorted(os.listdir(RECENT_ROOT))[:num_excess]  # WOO!! ISO 8601 GO BRRR
+
+        targets = []
+        for fn in oldest_files:
+            console.log("Scheduling", fn, "to be archived.", highlight=False)
+            targets.append(os.path.join(RECENT_ROOT, fn))
+        video_processor.map(archive_video, targets)
+
+    # Generate Survey Gifs: Recent c> Survey
+    targets = []
+    for fn in sorted(os.listdir(RECENT_ROOT), reverse=True):
+        # Generate survey gifs
+        if not os.path.exists(get_survey_name(fn)):
+            console.log("Scheduling survey gif generation for", fn, ".", highlight=False)
+            targets.append(os.path.join(RECENT_ROOT, fn))
+    video_processor.map(gengif, targets)
+
+    # Limit Number of Survey Gifs: Survey -> (deleted)
+    if len(os.listdir(SURVEY_ROOT)) > MAX_SURVEY_GIFS:
+        # Too many survey gifs
+        #console.log("Too many survey gifs.")
+        num_excess = len(os.listdir(SURVEY_ROOT)) - MAX_SURVEY_GIFS
+        #console.log("Deleting the oldest", num_excess, highlight=False)
+        oldest_files = sorted(os.listdir(SURVEY_ROOT))[:num_excess]  # WOO!! ISO 8601 GO BRRR
+        for fn in oldest_files:
+            console.log("Removed old survey gif:", fn, highlight=False)
+            os.remove(os.path.join(SURVEY_ROOT, fn))
+
+# Go!
+progress.start()
+with ThreadPool(1) as video_processor:
+    while True:
+        _thread.start_new_thread(update_video_state, ())  # Wheee
+        
+        # Write output
+        current_file_name = time.strftime(FILENAME_FMT) + ".ts"
+        current_file_name = os.path.join(LATEST_ROOT, current_file_name)
+
+        tid = progress.add_task(description=f"Recording {os.path.basename(current_file_name)}...", total=None)
+        try:
+            ffmpeg = subprocess.Popen([EXECUTABLE,
+                                             "-f", DRIVER,
+                                       #"-framerate", "15",
+                                             *FFMPEG_DEVICE_ARGS,
+
+                                             "-y",
+
+                                             "-t", str(MAX_FULLFILE_LEN),
+
+                                             "-vf", "drawtext=text=%{localtime}:x=w-tw-10:y=h-th-10:fontcolor=Yellow:fontsize=24:fontfile=C:/Windows/Fonts/arialbd.ttf"
+                                                    ",fps=10",
+                                             
+                                             "-c:v", "h264",
+                                             "-c:a", "aac",
+                                             "-f", "mpegts",
+                                             current_file_name
+                                             ],
+                                            stderr=subprocess.DEVNULL)
+            ffmpeg.wait()
+        except KeyboardInterrupt:
+            progress.remove_task(tid)
+            console.log("[yellow]Recording interrupted by user.")
+            console.log("Recording Finished:", os.path.basename(current_file_name), highlight=False)
+            update_video_state()
+            sys.exit()
+        assert ffmpeg.returncode == 0
+        progress.remove_task(tid)
+        console.log("Recording Finished:", os.path.basename(current_file_name), highlight=False)
---
+++ webinterface.py
@@ -0,0 +1,104 @@
+import subprocess
+import time
+import os
+
+from globalsettings import OUTFILES_ROOT, RECENT_ROOT, ARCHIVE_ROOT, SURVEY_ROOT, LATEST_ROOT, FILENAME_FMT
+
+import asyncio
+import tornado
+import hashlib
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("web/index.html")
+
+BUFFER_SIZE = 20*188
+class LiveHandler(tornado.web.RequestHandler):
+    def get_most_recent(self):
+        return os.path.join(LATEST_ROOT, sorted(os.listdir(LATEST_ROOT),reverse=True)[0])
+        
+    async def get(self):
+        mostrecent = self.get_most_recent()
+        with open(mostrecent, "rb") as f:
+            # Start from the latest point if we're only just starting stream
+            try:
+                f.seek(-BUFFER_SIZE, os.SEEK_END)
+            except OSError:
+                f.seek(0, os.SEEK_END)
+            cur_pos = f.tell()
+        while True:
+            while self.get_most_recent() == mostrecent:
+                # Attempt to find any more bytes (if there are any)
+                try:
+                    with open(mostrecent, "rb") as f:
+                        f.seek(cur_pos)
+                        while (data := f.read(BUFFER_SIZE)):  # Read the new data
+                            self.write(data)
+                            cur_pos += len(data)
+                            await self.flush()
+                except (OSError, FileNotFoundError) as e:
+                    if "Stream is closed" in str(e): raise
+                    print("OS Error while streaming:", e)
+                    await asyncio.sleep(1)
+                    continue
+                await asyncio.sleep(0.5)  # We're done here
+            
+            # Newer file created. Close connection.
+            return
+
+class RecentListHandler(tornado.web.RequestHandler):
+    def get(self):
+        recent = []
+        h = 0
+        for fn in sorted(os.listdir(RECENT_ROOT), reverse=True):
+            data = {
+                "id": fn,
+                "src": "/v/recent/" + fn,
+                "thumb": "/v/thumbGIF/" + os.path.splitext(fn)[0] + ".gif",
+                "title": time.strftime("%a %d %b, %H:%M", time.strptime(os.path.splitext(os.path.basename(fn))[0], FILENAME_FMT))
+                }
+            
+            recent.append(data)
+            h += hash(fn)
+        self.write({"value": recent, "hash": h})
+
+
+class FastMediaHandler(tornado.web.StaticFileHandler):
+    # These two functions' existence speeds things up significantly and i have no clue how
+    async def _flush(self, *args, **kwargs):
+        # Call parent
+        rval = await super().flush(*args, **kwargs)
+        await asyncio.sleep(0)  # Let's not block the entire fucking application, tornado
+        return rval  # Return value
+
+    def compute_etag(self):
+        if hasattr(self, "_item"):  # You want a hash? I'll give you a hash
+            if self._item.metadata.md5:
+                return self._item.metadata.md5
+            
+        hasher = hashlib.sha1()
+        for part in self._write_buffer:
+            hasher.update(part)
+        return '"%s"' % hasher.hexdigest()
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/live.ts", LiveHandler),
+
+        (r"/api/recent.json", RecentListHandler),
+        
+        (r"/resources/(.*)", FastMediaHandler, {"path": "web/resources"}),
+        (r"/v/recent/(.*)", FastMediaHandler, {"path": RECENT_ROOT}),
+        (r"/v/thumbGIF/(.*)", FastMediaHandler, {"path": SURVEY_ROOT}),
+        (r"/v/old/(.*)", FastMediaHandler, {"path": ARCHIVE_ROOT}),
+    ])
+
+async def main():
+    app = make_app()
+    app.listen(80)
+    shutdown_event = asyncio.Event()
+    await shutdown_event.wait()
+
+if __name__ == "__main__":
+    asyncio.run(main())
